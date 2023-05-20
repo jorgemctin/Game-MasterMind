ce650dd4f5a7fd14b8912b29e84beca376720073
Jorge Martin <jorgemctin@gmail.com>
2023-05-20 22:45:59 +0200

    fix colorpickers succesfully

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 
-let contenedores = document.getElementsByClassName("contenedor");
 
-let arrayContenedores = Array.from(contenedores);
+let colorpickers = document.getElementsByClassName("colorpicker");
+let colorsSelected = document.getElementsByClassName("colorSelected");
 
-arrayContenedores.map(
-    (elementoDentroDelArray, i) => {
+let arrayColor = Array.from(colorpickers);
+let arrayColor1 = Array.from(colorsSelected);
 
-        elementoDentroDelArray.addEventListener("mouseover", ()=> {
-            elementoDentroDelArray.classList.add("cercle");
-        })        
+// Verifica que haya la misma cantidad de colorpickers y colorSelected
+if (arrayColor.length === arrayColor1.length) {
+  // Itera sobre los elementos colorpicker
+        arrayColor.map((elementoDentroDelArray, i) => {
+        elementoDentroDelArray.addEventListener("click", () => {
+      // Acción que se realiza cuando se hace clic en el colorpicker
+        elementoDentroDelArray.querySelector("input[type='color']").click();
+    });
 
-        elementoDentroDelArray.addEventListener("mouseout", ()=> {
-            elementoDentroDelArray.classList.remove("cercle");
-        })        
-    } 
-);
+        elementoDentroDelArray.addEventListener("input", (event) => {
+      // Obtener el color seleccionado del color picker
+            const colorSelector = event.target.value;
+
+      // Aplicar el color seleccionado al elemento correspondiente
+            arrayColor1[i].style.backgroundColor = colorSelector;
+        });
+    });
+} else {
+    console.log("La cantidad de colores seleccionados no coincide.");
+}
 
 
-const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink"];
 
-const colorElements = colors.map((color, index) => {
-    return <div style={{ backgroundColor: color }}>{color}</div>;
-});
+
+
+
+
+
