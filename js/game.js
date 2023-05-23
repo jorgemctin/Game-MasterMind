4a5ca3e2760e1a635faf97999e3ab7c4661e164e
Jorge Martin <jorgemctin@gmail.com>
2023-05-23 20:14:22 +0200

    finish game board and add addEventListener for the Click

--- js/game.js
+++ js/game.js
@@ -3,8 +3,6 @@ let game1 = document.getElementById("gameline1");
 
 for(let i = 0; i < 6; i++) {
     let bolaElement = document.createElement('div')
-    // newDiv.id = 'contenedores';
-    // bolaElement.textContent = "";
     bolaElement.classList.add("gameline1");
     bolaElement.id = `bola-${i}`
     game1.appendChild(bolaElement)
@@ -35,3 +33,52 @@ let game4 = document.getElementById("gameline4");
         bolaElement.id = `bola-${i}`;
         game4.appendChild(bolaElement);
     }
+
+let game5 = document.getElementById("gameline5");
+    for(let i = 0; i < 6; i++) {
+        let bolaElement = document.createElement('div');
+        bolaElement.classList.add("gameline5");
+        bolaElement.id = `bola-${i}`;
+        game5.appendChild(bolaElement);
+    }
+
+let game6 = document.getElementById("gameline6");
+    for(let i = 0; i < 6; i++) {
+        let bolaElement = document.createElement('div');
+        bolaElement.classList.add("gameline6");
+        bolaElement.id = `bola-${i}`;
+        game6.appendChild(bolaElement);
+    }
+
+let game7 = document.getElementById("gameline7");
+    for(let i = 0; i < 6; i++) {
+        let bolaElement = document.createElement('div');
+        bolaElement.classList.add("gameline7");
+        bolaElement.id = `bola-${i}`;
+        game7.appendChild(bolaElement);
+    }
+
+let game8 = document.getElementById("gameline8");
+    for(let i = 0; i < 6; i++) {
+        let bolaElement = document.createElement('div');
+        bolaElement.classList.add("gameline8");
+        bolaElement.id = `bola-${i}`;
+        game8.appendChild(bolaElement);
+    }
+
+let game9 = document.getElementById("gameline9");
+    for(let i = 0; i < 6; i++) {
+        let bolaElement = document.createElement('div');
+        bolaElement.classList.add("gameline9");
+        bolaElement.id = `bola-${i}`;
+        game9.appendChild(bolaElement);
+    }
+
+let game10 = document.getElementById("gameline10");
+    for(let i = 0; i < 6; i++) {
+        let bolaElement = document.createElement('div');
+        bolaElement.classList.add("gameline10");
+        bolaElement.id = `bola-${i}`;
+        game10.appendChild(bolaElement);
+    }
+
