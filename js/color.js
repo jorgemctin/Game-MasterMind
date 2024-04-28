ba0bf64a5f8f0d66b76211d1b2dc99cd58ca94ba
Jorge Martin <jorgemctin@gmail.com>
2024-04-28 03:54:51 +0200

    Refactor: color code

--- js/color.js
+++ js/color.js
@@ -1,36 +1,34 @@
 
+const colorPickers = document.querySelectorAll('.colorPicker');
 //DEPENDS OF THE LEVEL SELECTED, SHOW 4,5 or 6 INPUT COLORs
 const selectingColors = () => {
   const amountColorsString = localStorage.getItem("selectLevelColors");
   const amountColors = parseInt(amountColorsString);
 
-  const colorPickers = document.querySelectorAll('.colorPicker');
-
   colorPickers.forEach((picker, i) => {
     if (i < amountColors) {
-      picker.style.display = 'block'; 
+      picker.style.display = 'block';
     } else {
-      picker.style.display = 'none'; 
+      picker.style.display = 'none';
     }
   });
 };
-
 selectingColors();
 
 //SENDING COLORS TO THE LOCAL STORAGE
 const storeColors = () => {
   const selectedColors = [];
-  colorInputs.forEach(input => {
-    selectedColors.push(input.value);
+  colorPickers.forEach(input => {
+    if (input.style.display !== 'none') {
+      selectedColors.push(input.querySelector('input').value);
+    }
   });
   localStorage.setItem("selectedColors", JSON.stringify(selectedColors));
 };
 
 const colorInputs = document.querySelectorAll("input[type='color']");
-
 colorInputs.forEach(input => {
-  input.addEventListener("change", (event) => {
-    console.log("input", event.target.value, event.target.id);
+  input.addEventListener("change", (e) => {
     storeColors();
   });
 });
